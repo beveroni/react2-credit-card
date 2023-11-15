0be31828a4bf5aae3b4ba4e19a5fd780669e1397
Veronika Bendova <svechovaver@gmail.com>
2023-11-15 13:32:22 +0100

    Task done v3

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './style.css';
 
 export const CreditCardInput = () => {
   const [inputs, setInputs] = useState(['', '', '', '']);
   const [isValid, setIsValid] = useState(true);
-  const inputRefs = [];
+  const inputRefs = useRef([]);
 
   const handleInputChange = (e, inputIndex) => {
     const value = e.target.value;
@@ -15,40 +15,40 @@ export const CreditCardInput = () => {
       setInputs(newInputs);
 
       if (value.length === 4 && inputIndex < 3) {
-        inputRefs[inputIndex + 1]?.focus();
+        inputRefs.current[inputIndex + 1]?.focus();
       }
     }
   };
 
   const setInputRef = (element) => {
-    inputRefs.push(element);
+    inputRefs.current.push(element);
   };
 
   useEffect(() => {
     const allInputsFilled = inputs.every((input) => input.length === 4);
     setIsValid(allInputsFilled);
+  }, [inputs]);
 
-    return (
-      <div className="credit-card-input-container">
-        {inputs.map((input, index) => (
-          <React.Fragment key={index}>
-            <input
-              className="credit-card-input"
-              type="text"
-              value={input}
-              onChange={(e) => handleInputChange(e, index)}
-              ref={setInputRef}
-              maxLength={4}
-            />
-            {index < 3 && <span> - </span>}
-          </React.Fragment>
-        ))}
-        {!isValid && (
-          <p className="credit-card-error-message">
-            Číslo kreditní karty není platné.
-          </p>
-        )}
-      </div>
-    );
-  });
+  return (
+    <div className="credit-card-input-container">
+      {inputs.map((input, index) => (
+        <React.Fragment key={index}>
+          <input
+            className="credit-card-input"
+            type="text"
+            value={input}
+            onChange={(e) => handleInputChange(e, index)}
+            ref={(el) => setInputRef(el)}
+            maxLength={4}
+          />
+          {index < 3 && <span> - </span>}
+        </React.Fragment>
+      ))}
+      {!isValid && (
+        <p className="credit-card-error-message">
+          Číslo kreditní karty není platné.
+        </p>
+      )}
+    </div>
+  );
 };
