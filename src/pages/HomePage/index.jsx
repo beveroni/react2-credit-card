8581ac554b423f3330a9bda78da410587530ad10
Veronika Bendova <svechovaver@gmail.com>
2023-11-15 12:42:23 +0100

    Task done

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,19 @@
+import './style.css';
+import { CreditCardInput } from '../../components';
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <header>
+        <h1>Kreditní karta</h1>
+      </header>
+      <p>Napište číslo své kreditní karty.</p>
+      <main>
+        <CreditCardInput />
+      </main>
+      <footer>
+        <p>Czechitas, Digitální akademie: Web</p>
+      </footer>
+    </div>
+  );
+};
